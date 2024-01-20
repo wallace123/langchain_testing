d710aef7f1573c5294c21bf21e443bc5ea33e7d8
wallace123 <wallace123@github.com>
2024-01-20 16:57:19 -0500

    updates

--- summarize.py
+++ summarize.py
@@ -1,4 +1,3 @@
-
 from decouple import config
 
 from langchain_openai import ChatOpenAI
@@ -19,8 +18,7 @@ Here is information related to information security: <info>{text}</info>
 Please do the following:
 1. Summarize the information at a high school reading level (in <summary> tags). \
 The summary should not exceed 150 words.
-2. Highlight key points that are relevant to cloud providers or Amazon Web Services (AWS) \
-(in <highlight> tags).
+2. Highlight key points that are relevant to cloud providers (in <highlight> tags).
 """
 print(template)
 prompt = ChatPromptTemplate.from_template(template)
